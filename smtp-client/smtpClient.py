a404adc398526b00b605f1a14059c25405c5a76f
codebleeder <sharad.shivmath@gmail.com>
2015-07-15 14:30:57 -0700

    added documentation; added running code of smtp-client

--- smtp-client/smtpClient.py
+++ smtp-client/smtpClient.py
@@ -1,105 +1,107 @@
-__author__ = 'cloudera'
-Skeleton Python Code for the
-Mail Client
-f
-rom socket import *
-msg
-=
-”
-\
-r
-\
-n I love computer networks!”
-endmsg
-=
-”
-\
-r
-\
-n.
-\
-r
-\
-n”
-#
-C
-hoose a mail server (e.g.
-Google
-mail
-server) and call it mailserver
-mailserver =
-#Fill in start #Fill in end
-#
-C
-reate socket called clientSocket and establish a TCP connection with mailserver
-#Fill in start
-#Fill in end
-r
-ecv
-=
-clientSocket.recv(1024)
+__author__ = 'sharads'
+
+import socket
+import base64
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+
+recipient = "<sss617@nyu.edu>"
+sender = "<sharad.shivmath@gmail.com>"
+username = "sharad.shivmath"
+password = 'password' # password string modified
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = "smtp.gmail.com"
+port = 587
+
+# Create socket called clientSocket and establish a TCP connection with mailserver
+clientSocket = socket.socket()
+clientSocket.connect((mailserver, port))
+recv = clientSocket.recv(1024)
 print recv
-if recv[:3]
-!=
-'220':
-print '220 reply not received from server.'
-#
-Send HELO command and print server response.
-heloCommand
-=
-'HELO Alice
-\
-r
-\
-n'
-clientSocket.send(heloCommand)
-recv1
-=
-clientSocket.recv(1024)
+if recv[:3] != '220':
+    print '220 reply not received from server.'
+
+# Send HELO command and print server response.
+clientSocket.send('HELO Alice\r\n')
+recv1 = clientSocket.recv(1024)
 print recv1
-if recv1[:3]
-!=
-'250':
-print '250 reply not received from server.'
-#
-Send MAIL FROM command and print server response.
-#
-Fill in start
-#
-Fill in end
+if recv1[:3] != '250':
+    print '250 reply not received from server.'
 
-#
-Send RCPT TO command and print server response.
-#
-Fill in start
-#
-Fill in end
-#
-Send DATA command and print server response.
-#
-Fill in start
-#
-Fill in end
-#
-S
-end message data.
-#
-Fill in start
-#
-Fill in end
+# Request an encrypted connection
+clientSocket.send('STARTTLS\r\n')
+tlsRecv = clientSocket.recv(1024)
+print tlsRecv
+if tlsRecv[:3] != '220':
+    print '220 reply not received from server'
 
-#
-M
-essage ends with a single period.
-#
-Fill in start
-#
-Fill in end
-#
-S
-end QUIT command and get server response.
-# F
-ill in
-start
-# F
-ill in end
+# Encrypt the socket
+ssl_clientSocket = socket.ssl(clientSocket)
+
+# Send the AUTH LOGIN command and print server response.
+ssl_clientSocket.write('AUTH LOGIN\r\n')
+authRecv = ssl_clientSocket.read(1024)
+print authRecv
+if authRecv[:3] != '334':
+    print '334 reply not received from server'
+
+# Send username and print server response.
+uname = base64.b64encode(username) + '\r\n'
+ssl_clientSocket.write(uname)
+unameRecv = ssl_clientSocket.read(1024)
+print unameRecv
+if unameRecv[:3] != '334':
+    print '334 reply not received from server'
+
+# Send password and print server response.
+pword = base64.b64encode(password) + '\r\n'
+ssl_clientSocket.write(pword)
+pwordRecv = ssl_clientSocket.read(1024)
+print pwordRecv
+if pwordRecv[:3] != '235':
+    print '235 reply not received from server'
+
+# Send MAIL FROM command and print server response.
+mailFromCommand = 'MAIL FROM: ' + sender + '\r\n'
+ssl_clientSocket.write(mailFromCommand)
+recv2 = ssl_clientSocket.read(1024)
+print recv2
+if recv2[:3] != '250':
+    print '250 reply not received from server.'
+
+# Send RCPT TO command and print server response.
+rcptToCommand = 'RCPT TO: ' + recipient + '\r\n'
+ssl_clientSocket.write(rcptToCommand)
+recv3 = ssl_clientSocket.read(1024)
+print recv3
+if recv3[:3] != '250':
+    print '250 reply not received from server.'
+
+# Send DATA command and print server response.
+dataCommand = 'DATA\r\n'
+ssl_clientSocket.write(dataCommand)
+recv4 = ssl_clientSocket.read(1024)
+print recv4
+if recv4[:3] != '354':
+    print '354 reply not received from server.'
+
+# Send message data.
+ssl_clientSocket.write(msg)
+
+# Message ends with a single period.
+ssl_clientSocket.write(endmsg)
+recv5 = ssl_clientSocket.read(1024)
+print recv5
+if recv5[:3] != '250':
+    print '250 reply not received from server.'
+
+# Send QUIT command and get server response.
+ssl_clientSocket.write('QUIT\r\n')
+recv6 = ssl_clientSocket.read(1024)
+print recv6
+if recv6[:3] != '221':
+    print '221 reply not received from server.'
+
+clientSocket.close()
--- udp-pinger/UDPPingerClient.py
+++ udp-pinger/UDPPingerClient.py
@@ -1,10 +1,30 @@
-__author__ = 'cloudera'
+__author__ = 'sharad'
 from datetime import datetime
 from socket import *
 from time import time
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = 'randomnumb'
-counter = 10
+counter = 0
+rtt_list = []
 while counter < 10:
+    # send one letter at a time
+    clientSocket.sendto(message[counter], ('localhost', 12000))
+    t1 = datetime.now()
+    # wait for one second
+    clientSocket.settimeout(1)
+    try:
+        modified_message, server_address = clientSocket.recvfrom(1024)
+        t2 = datetime.now()
+        rtt = (t2-t1).microseconds
+        print modified_message+' RTT = ', rtt
+        rtt_list.append(rtt)
+    except timeout:
+        print 'time out!'
     counter = counter + 1
+
+print 'counter end'
+print 'max RTT = ', max(rtt_list)
+print 'min RTT = ', min(rtt_list)
+print 'avg RTT = ', sum(rtt_list)/len(rtt_list)
+clientSocket.close()
